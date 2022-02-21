c2b589507e9ad82b17bc156b9cf4fed843b061f8
Santiago Julian <sjulian983@gmail.com>
2022-02-21 12:42:01 +0100

    detail page updated

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const [recipe, setRecipe] = useState([]);
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(
+      `https://api.spoonacular.com/recipes/${params.id}/information?includeNutrition=false&apiKey=${API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setRecipe(data));
+  }, []);
+  console.log(recipe);
+
+  return (
+    <div className="recipe-info">
+      <div className="recipe-info-card">
+        <img
+          src={recipe.image}
+          alt="recipe description"
+          className="description-image"
+        />
+        <h3>{recipe.title}</h3>
+        <h4>{`Vegan: ${recipe.vegan}`}</h4>
+        <h4>{`Vegetarian: ${recipe.vegetarian}`}</h4>
+        <h4>{`Servings: ${recipe.servings}`}</h4>
+        <h4>Ingredients:</h4>
+
+        <p>{`Instructions: ${recipe.instructions}`}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = ({ recipes }) => {
-  const showDetails = () => {
-    console.log("clicked");
-  };
-
   return (
     <div className="home">
       {recipes.map((recipe) => {
@@ -19,7 +16,9 @@ const Home = ({ recipes }) => {
             <h4>{`Vegan: ${recipe.vegan}`}</h4>
             <h4>{`Vegetarian: ${recipe.vegetarian}`}</h4>
             <h4>{`Servings: ${recipe.servings}`}</h4>
-            <button onClick={showDetails}>More Details</button>
+            <button>
+              <Link to={`/details/${recipe.id}`}>More Details</Link>
+            </button>
           </div>
         );
       })}
