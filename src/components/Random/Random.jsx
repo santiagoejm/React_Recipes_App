2c66589d69449fb3005550003837959c19c439f5
Santiago Julian <sjulian983@gmail.com>
2022-02-25 14:58:52 +0100

    home categories and random pages fetching

--- src/components/Random/Random.jsx
+++ src/components/Random/Random.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { myContext } from "../context/context";
+import "./random.scss";
 
 const Random = () => {
-  return <div>Random</div>;
+  const { random, fetchRandom } = useContext(myContext);
+
+  useEffect(() => {
+    fetchRandom();
+  }, [fetchRandom]);
+
+  return (
+    <div className="random">
+      {random.map((meal) => {
+        return (
+          <div className="random-grid" key={meal.idMeal}>
+            <div className="random-grid-controls">
+              <img src={meal.strMealThumb} alt="#" />
+              <button onClick={fetchRandom}>New meal</button>
+            </div>
+
+            <div className="random-grid-instructions">
+              <h4>Intructions</h4>
+              <p>{meal.strInstructions}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Random;
